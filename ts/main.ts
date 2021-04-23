56c05ffca975e9727358dc0adf706dd14900201b
timothyfox443 <52527132+timothyfox443@users.noreply.github.com>
2021-04-22 23:32:50 -0700

    created main.ts

---
+++ ts/main.ts
@@ -0,0 +1,10 @@
+window.onload = function()
+{
+    let formBtn = <HTMLElement>document.querySelector("form > button");
+    formBtn.onclick = main;
+}
+
+function main():void
+{
+    alert("Reg button was clicked");
+}
