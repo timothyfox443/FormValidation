3a3c38405cd23d5afcb1bdb1d581c09e4e83c6ff
timothyfox443 <52527132+timothyfox443@users.noreply.github.com>
2021-05-07 21:44:49 -0700

    Fleshed out more on the ts file it does something now

--- js/main.js
+++ js/main.js
@@ -1,4 +1,40 @@
+var MarketGoods = (function () {
+    function MarketGoods() {
+    }
+    return MarketGoods;
+}());
 window.onload = function () {
-    var addBtn = document.querySelector("input[type=button]");
-    addBtn.onclick = addVideoGame;
+    var addBtn = document.getElementById("submitBtn");
+    addBtn.onclick = addProduct;
+};
+function addProduct() {
+    alert("You submitted this");
+    var addThis = getMarketGoods();
+    showGoods(addThis);
+}
+function getMarketGoods() {
+    var data = new MarketGoods();
+    data.name = getInputById("name").value;
+    data.price = parseFloat(getInputById("price").value);
+    data.description = getInputById("description").value;
+    data.expDate = getInputById("expDate").value;
+    return data;
+}
+function showGoods(showMe) {
+    var formOutput = getInputById("formOutput");
+    var productName = document.createElement("p");
+    productName.innerHTML = "Name: " + showMe.name;
+    var productPrice = document.createElement("p");
+    productPrice.innerHTML = "Price: " + showMe.price;
+    var productDescription = document.createElement("p");
+    productDescription.innerHTML = "Description: " + showMe.description;
+    var productExpDate = document.createElement("p");
+    productExpDate.innerHTML = "Expiration Date: " + showMe.expDate;
+    formOutput.appendChild(productName);
+    formOutput.appendChild(productPrice);
+    formOutput.appendChild(productDescription);
+    formOutput.appendChild(productExpDate);
+}
+var getInputById = function (id) {
+    return document.getElementById(id);
 };
--- ts/main.ts
+++ ts/main.ts
@@ -1,11 +1,58 @@
+class MarketGoods
+{
+    name: string;
+    price: number;
+    description: string;
+    expDate: string;
+}
 window.onload = function()
 {
-    let addBtn = <HTMLElement>document.querySelector("input[type=button]");
-    addBtn.onclick = addVideoGame;
+    let addBtn = <HTMLElement>document.getElementById("submitBtn");
+    addBtn.onclick = addProduct;
 }
 
-function addVideoGame():
+function addProduct()//what does this do?
 {
-    alert("Reg button was clicked");
+    alert("You submitted this");
+    let addThis:MarketGoods = getMarketGoods();
+    showGoods(addThis);
     
+}
+
+function getMarketGoods():MarketGoods //this gets all the data submitted by user
+{
+    let data = new MarketGoods();
+    data.name = getInputById("name").value;
+    data.price = parseFloat(getInputById("price").value);
+    data.description = getInputById("description").value;
+    data.expDate = getInputById("expDate").value;
+    return data;
+}
+
+function showGoods(showMe:MarketGoods):void//show me the money
+{
+    let formOutput = getInputById("formOutput");
+    let productName = document.createElement("p");
+    productName.innerHTML = `Name: ${showMe.name}`;
+    
+    let productPrice = document.createElement("p");
+    productPrice.innerHTML = `Price: ${showMe.price}`;
+    
+    let productDescription = document.createElement("p");
+    productDescription.innerHTML = `Description: ${showMe.description}`;
+
+    let productExpDate = document.createElement("p");
+    productExpDate.innerHTML = `Expiration Date: ${showMe.expDate}`;
+
+    formOutput.appendChild(productName);
+    formOutput.appendChild(productPrice);
+    formOutput.appendChild(productDescription);
+    formOutput.appendChild(productExpDate);
+
+
+}
+
+
+var getInputById = function(id:string){
+    return (<HTMLInputElement>document.getElementById(id));
 }
